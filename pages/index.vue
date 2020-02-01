02a79f71770c6169962c4fd7ab38fe6453a51c9b
alfredo fernandez <alfredofernandezcarlos@gmail.com>
2020-02-01 13:11:44 -0400

    s4/120 Move parts of App to components

---
+++ pages/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <hero />
+    <section class="section">
+      <div class="container">
+        <h1 class="title">Featured Courses</h1>
+        <div class="columns">
+          <div class="column is-one-quarter">
+            <CourseCard />
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="section">
+      <div class="container">
+        <h1 class="title">Featured Articles</h1>
+        <div class="columns">
+          <div class="column is-one-quarter">
+            <BlogCard />
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import CourseCard from '~/components/shared/CourseCard'
+import BlogCard from '~/components/shared/BlogCard'
+import Hero from '~/components/shared/Hero'
+
+export default {
+  components: {
+    CourseCard, BlogCard, Hero
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  // Home page
+  .links {
+    padding-top: 15px;
+  }
+</style>
