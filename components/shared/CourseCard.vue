7ba95d7b673c64c4cc56149bea8bc834df53aa44
alfredo fernandez <alfredofernandezcarlos@gmail.com>
2020-02-01 20:55:52 -0400

    s4/126 Provide courses to Card

--- components/shared/CourseCard.vue
+++ components/shared/CourseCard.vue
@@ -2,23 +2,23 @@
   <div class="card">
     <div class="card-image">
       <figure class="image is-4by2">
-        <img src="https://images.unsplash.com/photo-1542435503-956c469947f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=667&q=80">
+        <img :src="course.image">
       </figure>
     </div>
     <div class="card-content">
       <div class="media">
         <div class="media-content">
-          <p class="title is-4">Some Super Title</p>
-          <p class="subtitle is-6"><i>Some Super Subtitle</i></p>
+          <p class="title is-4">{{ course.title }}</p>
+          <p class="subtitle is-6"><i>{{ course.author.name }}</i></p>
         </div>
       </div>
       <div class="content">
-        Some Description
+        {{ course.subtitle }}
         <br>
       </div>
       <div class="price-box">
-        <span class="price">150$</span>
-        <span class="disc-price">9.99$</span>
+        <span class="price">{{ course.price }}</span>
+        <span class="disc-price">{{ course.discountedPrice }}</span>
       </div>
     </div>
     <footer class="card-footer">
@@ -27,7 +27,17 @@
     </footer>
   </div>
 </template>
-
+<!-- credate script, specify props of course --->
+<script>
+export default {
+  props: {
+    course: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
 <style lang="scss" scope>
   .card-image:hover {
     cursor: pointer;
