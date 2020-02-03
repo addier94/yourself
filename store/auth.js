b335a4e0131e2c79d0b2abde3b607d19c5529382
alfredo fernandez <alfredofernandezcarlos@gmail.com>
2020-02-03 11:46:40 -0400

    s5/137 Display user in Navbar

--- store/auth.js
+++ store/auth.js
@@ -4,6 +4,20 @@ export const state = () => ({
   user: null
 })
 
+export const getters = {
+  authUser (state) {
+    return state.user || null
+  },
+  isAuthenticated(state) {
+    return !!state.user
+  },
+  isAdmin (state) {
+    return  state.user && 
+            state.user.role &&
+            state.user.role === 'admin'
+  }
+}
+
 export const actions = {
   login({commit, state}, loginData) {
     return this.$axios.$post('/api/v1/users/login', loginData)
