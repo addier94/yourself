c0184e3977e73261054091418da248cec6a85846
alfredo fernandez <alfredofernandezcarlos@gmail.com>
2020-02-06 15:23:59 -0400

    s6/160 Finishes input with character counter

--- components/form/TextInputWithCount.vue
+++ components/form/TextInputWithCount.vue
@@ -1,15 +1,25 @@
 <template>
+<div>
   <input
     :maxLength="maxLength"
-    @input="$event => $emit('input', $event.target.value)"
+    @input="emitInputValue"
     @blur="v.$touch()"
     type="text"
     placeholder="Titulo para el curso"
     class="input is-large">
+  <span class="icon is-small is-right">
+    {{ remainingLenght }}
+  </span>
+</div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      currentValue: ''
+    }
+  },
   props: {
     maxLength: {
       type: Number,
@@ -19,6 +29,26 @@ export default {
     v: {
       type: Object,
       required: true
+    }
+  },
+  computed: {
+    inputLength() {
+      return this.currentValue.length || 0
+    },
+    remainingLenght() {
+      if (this.inputLength > 0 && this.inputLength < this.maxLength) {
+        return this.maxLength - this.inputLength
+      } else if (this.inputLength === 0) {
+        return this.maxLength
+      } else {
+        return 0
+      }
+    }
+  },
+  methods: {
+    emitInputValue($event) {
+      this.currentValue = $event.target.value
+      this.$emit('input',this.currentValue)
     }
   }
 }
