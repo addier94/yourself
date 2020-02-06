c215a5fd5b2c3b45cc878ab75456aa290f9ac52b
alfredo fernandez <alfredofernandezcarlos@gmail.com>
2020-02-06 09:06:15 -0400

    s6/158 Init text input with character count

---
+++ components/form/TextInputWithCount.vue
@@ -0,0 +1,19 @@
+<template>
+  <input
+    :maxLength="maxLength"
+    type="text"
+    placeholder="Titulo para el curso"
+    class="input is-large">
+</template>
+
+<script>
+export default {
+  props: {
+    maxLength: {
+      type: Number,
+      default: 50,
+      required: false
+    }
+  }
+}
+</script>
