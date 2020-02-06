7a88a049ea3dc5cb7375de110fd33e40f3c4d760
alfredo fernandez <alfredofernandezcarlos@gmail.com>
2020-02-06 16:19:02 -0400

    s6/161 Create course - send a request

--- pages/instructor/courses/index.vue
+++ pages/instructor/courses/index.vue
@@ -37,12 +37,12 @@
                   <div class="columns">
                     <div class="column is-narrow">
                       <figure class="image is-4by2 is-128x128">
-                        <img :src="'https://i.udemycdn.com/course/750x422/2381802_d90c_3.jpg'">
+                        <img :src="course.image || 'https://via.placeholder.com/150'">
                       </figure>
                     </div>
                     <div class="column">
                       <p class="title">{{ course.title }}</p>
-                      <p class="subtitle">{{ course.subtitle }}</p>
+                      <p class="subtitle">{{ course.subtitle || 'Aun sin subtítulo' }}</p>
                       <span class="tag"
                             :class="'is-success'">{{ course.status }}</span>
                     </div>
