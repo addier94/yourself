869991d4953ff1cb7d8730f1985f8bd3bd9f8ca9
alfredo fernandez <alfredofernandezcarlos@gmail.com>
2020-03-30 22:09:32 -0400

    s8/213 Inject & Display featured blogs

--- store/blog.js
+++ store/blog.js
@@ -1,25 +1,4 @@
-const applyParamsToUrl = (url, filter) => {
-  if (filter) {
-    let filteredEntities = ''
-    if (url.indexOf('?') === -1) {
-      url += '?'
-    } else {
-      url += '&'
-    }
 
-    Object.keys(filter).forEach(key => {
-      filteredEntities += `${key}=${filter[key]}&`
-    })
-
-    if (filteredEntities.slice(-1) === '&') {
-      filteredEntities = filteredEntities.slice(0, -1)
-    }
-
-    return url + filteredEntities
-  }
-
-  return url
-}
 export const state = () => ({
   item: {},
   items: {
@@ -40,7 +19,7 @@ export const actions = {
   },
   // /api/v1/blogs?filter[featured]=true
   fetchFeaturedBlogs({commit, state}, filter) {
-    const url = applyParamsToUrl('/api/v1/blogs', filter)
+    const url = this.$applyParamsToUrl('/api/v1/blogs', filter)
     return this.$axios.$get(url)
       .then(data => {
         const { blogs } = data
