4cfa6640574989202cee13bf6e7c4b0eb8f4a011
alfredo fernandez <alfredofernandezcarlos@gmail.com>
2020-04-06 08:50:43 -0400

    118 Prod changes part 2

--- server/keys/prod.js
+++ server/keys/prod.js
@@ -1,9 +1,4 @@
-// module.exports = {
-//   DB_URI: process.env.DB_URI,
-//   SESSION_SECRET: process.env.SESSION_SECRET
-// }
-
 module.exports = {
-  DB_URI: 'mongodb+srv://testuser:@lfreddy878@cluster0-s12dh.mongodb.net/data-db?retryWrites=true&w=majority',
-  SESSION_SECRET: 'djskzclzvxv/fd,/fdm'
+  DB_URI: process.env.DB_URI,
+  SESSION_SECRET: process.env.SESSION_SECRET
 }
