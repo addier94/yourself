73df23f4791ffb47682bc643fa2500c2c2d814f3
alfredo fernandez <alfredofernandezcarlos@gmail.com>
2020-04-05 21:50:30 -0400

    s12/244 Deployment part 2

--- server/keys/prod.js
+++ server/keys/prod.js
@@ -1,4 +1,9 @@
+// module.exports = {
+//   DB_URI: process.env.DB_URI,
+//   SESSION_SECRET: process.env.SESSION_SECRET
+// }
+
 module.exports = {
-  DB_URI: process.env.DB_URI,
-  SESSION_SECRET: process.env.SESSION_SECRET
+  DB_URI: 'mongodb+srv://testuser:@lfreddy878@cluster0-s12dh.mongodb.net/data-db?retryWrites=true&w=majority',
+  SESSION_SECRET: 'djskzclzvxv/fd,/fdm'
 }
